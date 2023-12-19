ee2a9cc4f1ccab51df537d993e248d336a225040
FDomenico <domenicofiorente98@gmail.com>
2023-12-19 15:41:37 +0100

    Modifiche effettuate

--- src/Prepocessing.py
+++ src/Prepocessing.py
@@ -1,3 +1,42 @@
 import pandas as pd
+from Utils import *
+
+
+def preprocessing():
+    print("Reading data...")
+    violation = pd.read_csv('../dataset/traffic_violations.csv')
+    print("Preprocessing...")
+
+    drop_column_traffic = ['description', 'race', 'state', 'driver_city']
+    violation.drop(drop_column_traffic, axis=1, inplace=True)
+    column_check_na_traffic = ['belts', 'personal_injury', 'property_damage',
+                               'commercial_license', 'commercial_vehicle', 'vehicle_type',
+                               'year', 'make', 'model', 'color', 'charge', 'contributed_to_accident',
+                               'gender', 'driver_state', 'dL_state', 'arrest_type', 'violation_type']
+    violation.dropna(subset=column_check_na_traffic, inplace=True)
+
+    print("Preprocessing done!")
+
+    plot_violation_type(violation, save=False, show=True)
+
+    print("Balancing data...")
+    min_samples = violation['violation_type'].value_counts().min()
+    balanced_violation_type = pd.DataFrame()
+    for violation_type in violation['violation_type'].unique():
+        sampled_data = violation[violation['violation_type'] == violation_type].sample(n=min_samples, random_state=42)
+        balanced_violation_type = balanced_violation_type._append(sampled_data)
+
+    print("Balancing done!")
+    stats(violation, save=True, display=False)
+
+    plot_violation_type(balanced_violation_type)
+
+    return balanced_violation_type
+
+
+t = preprocessing()
+print("Saving preprocessed data...")
+t.to_csv(NEW_TRAFFIC_VIOLATIONS_PATH, index=False)
+print("Preprocessed data saved!")
 
 
--- src/Utils.py
+++ src/Utils.py
@@ -16,7 +16,25 @@ def plot_violation_type(violation: pd.DataFrame, save=True, show=False):
     plt.close()
 
 
-data = pd.read_csv('../dataset/traffic_violations.csv')
-plot_violation_type(data)
+def stats(violation: pd.DataFrame, save=True, display=True):
+    stat_g = violation[['gender']].describe()
+    stat_m = violation[['model']].describe()
+    stat_make = violation[['make']].describe()
+    stat_c = violation[['charge']].describe()
+    if display:
+        print(stat_g)
+        print(stat_m)
+        print(stat_make)
+        print(stat_c)
+    if save:
+        stat_g.to_csv("../Dataset/gender_stat.csv")
+        stat_m.to_csv("../Dataset/model_stat.csv")
+        stat_make.to_csv("../Dataset/make_stat.csv")
+        stat_c.to_csv("../Dataset/charge_stat.csv")
+
+
+
+#data = pd.read_csv('../dataset/traffic_violations.csv')
+#plot_violation_type(data)
 
 

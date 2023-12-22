ed58db89db4fec2274ed95cb34c6ca87c5e1744c
FDomenico <domenicofiorente98@gmail.com>
2023-12-22 10:20:27 +0100

    Scritte prime query

--- src/Utils.py
+++ src/Utils.py
@@ -29,21 +29,42 @@ def plot_violation_type(violation: pd.DataFrame, save=True, show=False):
     plt.close()
 
 
+def code_label_dict(field_name: str, inverse=False) -> dict:
+    df_guide = pd.read_excel('../Dataset/Road-Safety-Open-Dataset-Data-Guide.xlsx')
+    d = {}
+    for index, row in df_guide[df_guide['field name'] == field_name][['code/format', 'label']].iterrows():
+        k = row['code/format']
+        v = row['label']
+        if inverse:
+            d[v] = k
+        d[k] = v
+    return d
+
+
+def change_code_to_description_df(df: pd.DataFrame, inverse=False):
+    for col in df.columns:
+        df[col] = df[col].replace(code_label_dict(col, inverse))
+
+
 def stats(violation: pd.DataFrame, save=True, display=True):
     stat_g = violation[['gender']].describe()
     stat_m = violation[['model']].describe()
     stat_make = violation[['make']].describe()
     stat_c = violation[['charge']].describe()
+    stat_vt = violation['vehicle_type'].unique()
+    df_stat_vt = pd.DataFrame(stat_vt)
     if display:
         print(stat_g)
         print(stat_m)
         print(stat_make)
         print(stat_c)
+        print(stat_vt)
     if save:
         stat_g.to_csv("../Dataset/gender_stat.csv")
         stat_m.to_csv("../Dataset/model_stat.csv")
         stat_make.to_csv("../Dataset/make_stat.csv")
         stat_c.to_csv("../Dataset/charge_stat.csv")
+        df_stat_vt.to_csv("../Dataset/vehicle_type_stat.csv")
 
 
 def cross_val_score_plot(score, name, save: True, display: False):
